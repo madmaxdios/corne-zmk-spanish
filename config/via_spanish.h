bcc4aff4c472cdaf940526a9e1c9276093e79431
Victor DG <vitidg@gmail.com>
2023-05-21 11:26:06 +0200

    no us bslsh

--- config/via_spanish.h
+++ config/via_spanish.h
@@ -124,7 +124,7 @@
 #define KC_ACEN &kp APOS      // ´
 #define KC_CERI &kp HASH      // Ç
 // Row 4
-#define KC_LESS &kp BSLH // <
+#define KC_LESS &kp NON_US_BSLH // <
 #define KC_Z &kp Z        // Z
 #define KC_X &kp X        // X
 #define KC_C &kp C        // C
