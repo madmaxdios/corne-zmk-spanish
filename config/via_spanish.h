6b8b42dd432bae06ef6ff23a6c5d03c21e2c7b5c
vitidg <vitidg@gmail.com>
2023-05-24 14:00:05 +0200

    teclas es

--- config/via_spanish.h
+++ config/via_spanish.h
@@ -1,140 +1,175 @@
 // #pragma once
-#define KC_NO &none
-#define KC_TRNS &trans
+#define ES_NO &none
+#define ES_TRNS &trans
 
 // layers
 #define LAY_1 &mo 1
 #define LAY_2 &mo 2
 #define LAY_3 &mo 3
 
-
 // CONTROL CMD/WINDOWS SHIFT ALT/META/OPTION
-#define KC_LCTL &kp LCTRL // left control
-#define KC_RCTL &kp RCTRL // right control
-#define KC_WIN &kp LGUI   // WIN
-#define KC_CMENU &kp K_CMENU   // WIN
-#define KC_LALT &kp LALT
-#define KC_ALGR &kp RALT
-#define KC_LSFT &kp LSHFT
-#define KC_RSFT &kp RSHFT
+#define ES_LCTL &kp LCTRL // left control
+#define ES_RCTL &kp RCTRL // right control
+#define ES_WIN &kp LGUI   // WIN
+#define ES_CMENU &kp K_CMENU   // WIN
+#define ES_LALT &kp LALT
+#define ES_ALGR &kp RALT
+#define ES_LSFT &kp LSHFT
+#define ES_RSFT &kp RSHFT
 
 // ARROWS
-#define KC_UP &kp UP
-#define KC_LEFT &kp LEFT
-#define KC_DOWN &kp DOWN
-#define KC_RIGHT &kp RIGHT
+#define ES_UP &kp UP
+#define ES_LEFT &kp LEFT
+#define ES_DOWN &kp DOWN
+#define ES_RIGHT &kp RIGHT
 
 // SPACE CAPS TAB ENTER BACKSPACE ESCAPE DELETE
-#define KC_SPC &kp SPACE // Space
-#define KC_CAPS &kp CAPS   // Caps Lock
-#define KC_BSPC &kp BSPC   // Backspace
-#define KC_ENT &kp RET     // Enter
-#define KC_TAB &kp TAB     // Tab
-#define KC_DEL &kp DEL     // Delete
+#define ES_SPC &kp SPACE // Space
+#define ES_CAPS &kp CAPS   // Caps Lock
+#define ES_BSPC &kp BSPC   // Backspace
+#define ES_ENT &kp RET     // Enter
+#define ES_TAB &kp TAB     // Tab
+#define ES_DEL &kp DEL     // Delete
 
 // PAGE UP PAGE DOWN HOME END
-#define KC_PGUP &kp PG_UP
-#define KC_PGDN &kp PG_DN
-#define KC_HOME &kp HOME
-#define KC_END &kp END
-#define KC_INS &kp END
-#define KC_PRINT &kp PSCRN
-#define KC_PAUSE &kp PAUSE_BREAK
+#define ES_PGUP &kp PG_UP
+#define ES_PGDN &kp PG_DN
+#define ES_HOME &kp HOME
+#define ES_END &kp END
+#define ES_INS &kp END
+#define ES_PRINT &kp PSCRN
+#define ES_PAUSE &kp PAUSE_BREAK
 
 
 
 // FUNCTION KEYS
-#define KC_ESC &kp ESC
-#define KC_F1 &kp F1
-#define KC_F2 &kp F2
-#define KC_F3 &kp F3
-#define KC_F4 &kp F4
-#define KC_F5 &kp F5
-#define KC_F6 &kp F6
-#define KC_F7 &kp F7
-#define KC_F8 &kp F8
-#define KC_F9 &kp F9
-#define KC_F10 &kp F10
-#define KC_F11 &kp F11
-#define KC_F12 &kp F12
+#define ES_ESC &kp ESC
+#define ES_F1 &kp F1
+#define ES_F2 &kp F2
+#define ES_F3 &kp F3
+#define ES_F4 &kp F4
+#define ES_F5 &kp F5
+#define ES_F6 &kp F6
+#define ES_F7 &kp F7
+#define ES_F8 &kp F8
+#define ES_F9 &kp F9
+#define ES_F10 &kp F10
+#define ES_F11 &kp F11
+#define ES_F12 &kp F12
 
 // SOUND / VOLUME
-#define KC_VOL_UP &kp C_VOLUME_UP
-#define KC_VOL_DN &kp C_VOLUME_DOWN
-#define KC_MUTE &kp C_MUTE
-#define KC_NEXT &kp C_NEXT
-#define KC_PREV &kp C_PREVIOUS
-#define KC_STOP &kp C_STOP
-#define KC_PLAY &kp C_PLAY_PAUSE
-#define KC_SHUFFLE &kp C_SHUFFLE
+#define ES_VOL_UP &kp C_VOLUME_UP
+#define ES_VOL_DN &kp C_VOLUME_DOWN
+#define ES_MUTE &kp C_MUTE
+#define ES_NEXT &kp C_NEXT
+#define ES_PREV &kp C_PREVIOUS
+#define ES_STOP &kp C_STOP
+#define ES_PLAY &kp C_PLAY_PAUSE
+#define ES_SHUFFLE &kp C_SHUFFLE
 
 // DISPLAY BRIGHTNESS
-#define KC_BRI_UP &kp C_BRI_UP
-#define KC_BRI_DN &kp C_BRI_DN
+#define ES_BRI_UP &kp C_BRI_UP
+#define ES_BRI_DN &kp C_BRI_DN
 
 // LANGUAGE
-#define KC_LANG1 &kp LANG1
-#define KC_LANG2 &kp LANG2
-#define KC_LANG3 &kp LANG3
-#define KC_LANG4 &kp LANG4
-#define KC_LANG5 &kp LANG5
-#define KC_LANG6 &kp LANG6
-#define KC_LANG7 &kp LANG7
-#define KC_LANG8 &kp LANG8
-#define KC_LANG9 &kp LANG9
+#define ES_LANG1 &kp LANG1
+#define ES_LANG2 &kp LANG2
+#define ES_LANG3 &kp LANG3
+#define ES_LANG4 &kp LANG4
+#define ES_LANG5 &kp LANG5
+#define ES_LANG6 &kp LANG6
+#define ES_LANG7 &kp LANG7
+#define ES_LANG8 &kp LANG8
+#define ES_LANG9 &kp LANG9
 
 // Row 1
-#define KC_CIRC &kp GRAVE // º
-#define KC_1 &kp N1       // 1
-#define KC_2 &kp N2       // 2
-#define KC_3 &kp N3       // 3
-#define KC_4 &kp N4       // 4
-#define KC_5 &kp N5       // 5
-#define KC_6 &kp N6       // 6
-#define KC_7 &kp N7       // 7
-#define KC_8 &kp N8       // 8
-#define KC_9 &kp N9       // 9
-#define KC_0 &kp N0       // 0
-#define KC_APOS &kp MINUS   // '
-#define KC_EXCL &kp EQUAL // ¡
+#define ES_CIRC &kp GRAVE // º
+#define ES_1 &kp N1       // 1
+#define ES_2 &kp N2       // 2
+#define ES_3 &kp N3       // 3
+#define ES_4 &kp N4       // 4
+#define ES_5 &kp N5       // 5
+#define ES_6 &kp N6       // 6
+#define ES_7 &kp N7       // 7
+#define ES_8 &kp N8       // 8
+#define ES_9 &kp N9       // 9
+#define ES_0 &kp N0       // 0
+#define ES_APOS &kp MINUS   // ' -
+#define ES_EXCL &kp EQUAL // ¡ =
+
 // Row 2
-#define KC_Q &kp Q       // Q
-#define KC_W &kp W       // W
-#define KC_E &kp E       // E
-#define KC_R &kp R       // R
-#define KC_T &kp T       // T
-#define KC_Y &kp Y       // Y
-#define KC_U &kp U       // U
-#define KC_I &kp I       // I
-#define KC_O &kp O       // O
-#define KC_P &kp P       // P
-#define KC_ACEN_R &kp LBKT // `
-#define KC_PLUS &kp RBKT // +
+#define ES_Q &kp Q       // Q
+#define ES_W &kp W       // W
+#define ES_E &kp E       // E
+#define ES_R &kp R       // R
+#define ES_T &kp T       // T
+#define ES_Y &kp Y       // Y
+#define ES_U &kp U       // U
+#define ES_I &kp I       // I
+#define ES_O &kp O       // O
+#define ES_P &kp P       // P
+#define ES_ACEN_R &kp LBKT // ` [
+#define ES_PLUS &kp RBKT // + ]
+
 // Row 3
-#define KC_A &kp A            // A
-#define KC_S &kp S            // S
-#define KC_D &kp D            // D
-#define KC_F &kp F            // F
-#define KC_G &kp G            // G
-#define KC_H &kp H            // H
-#define KC_J &kp J            // J
-#define KC_K &kp K            // K
-#define KC_L &kp L            // L
-#define KC_EGNE &kp SEMICOLON // Ñ
-#define KC_ACEN &kp APOS      // ´
-#define KC_CERI &kp HASH      // Ç
+#define ES_A &kp A            // A
+#define ES_S &kp S            // S
+#define ES_D &kp D            // D
+#define ES_F &kp F            // F
+#define ES_G &kp G            // G
+#define ES_H &kp H            // H
+#define ES_J &kp J            // J
+#define ES_K &kp K            // K
+#define ES_L &kp L            // L
+#define ES_EGNE &kp SEMICOLON // Ñ ;
+#define ES_ACEN &kp APOS      // ´ '
+#define ES_CERI &kp HASH      // Ç bslh
+
 // Row 4
-#define KC_LESS &kp NON_US_BSLH // <
-#define KC_Z &kp Z        // Z
-#define KC_X &kp X        // X
-#define KC_C &kp C        // C
-#define KC_V &kp V        // V
-#define KC_B &kp B        // B
-#define KC_N &kp N        // N
-#define KC_M &kp M        // M
-#define KC_COMM &kp COMMA // ,
-#define KC_DOT &kp DOT    // .
-#define KC_MINUS &kp KP_MINUS // -
+#define ES_LESS &kp BSLSH // < bslsh
+#define ES_LESS_R &kp LS(BSLSH) // < bslsh
+#define ES_Z &kp Z        // Z
+#define ES_X &kp X        // X
+#define ES_C &kp C        // C
+#define ES_V &kp V        // V
+#define ES_B &kp B        // B
+#define ES_N &kp N        // N
+#define ES_M &kp M        // M
+#define ES_COMM &kp COMMA // , ,
+#define ES_DOT &kp DOT    // . .
+#define ES_DASH &kp KP_MINUS // - /
+
+// symbols shift
+#define ES_EXCL_R &kp LS(N1) // !
+#define ES_DOUB_APO &kp LS(N2) // "
+#define ES_DOT_MID &kp LS(N3) // ·
+#define ES_DOLL &kp LS(N4) // $
+#define ES_PERC &kp LS(N5) // %
+#define ES_AMPER &kp LS(N6) // & 
+#define ES_SLASH &kp LS(N7) // /
+#define ES_PAREN &kp LS(N8) // (
+#define ES_PAREN_R &kp LS(N9) // )
+#define ES_EQUAL &kp LS(N0) // =
+#define ES_QUEST_R &kp LS(MINUS) // ?
+#define ES_QUEST &kp LS(EQUAL) // ¿
+
+#define ES_ACEN_CIR &kp LS(LBKT) // ^
+#define ES_STAR &kp LS(RBKT) // *
+
+// symbol r alt
+#define ES_BSLSH &kp RA(GRAVE) // BSLSH
+#define ES_PIPE &kp RA(N1) // |
+#define ES_AT &kp RA(N2) // @
+#define ES_HAS &kp RA(N) // #
+#define ES_NOT &kp RA(N6) // ¬
+
+#define ES_CORCH &kp RA(LBKT) // [
+#define ES_CORCH_R &kp RA(RBKT) // ]
+#define ES_LLAVE &kp RA(APOS) // {
+#define ES_LLAVE_R &kp RA(HASH) // }
+
+#define ES_EURO &kp RA(E) // €
+
 
 
 // mt
